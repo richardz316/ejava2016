dd759531a559d390f065555214344a076f545cb5
richardz316 <jsman316@gmail.com>
2016-10-22 14:40:13 +0800

    -add AppConfig for rest

---
+++ src/java/sg/edu/nus/iss/ejava/ca/rest/PeopleResource.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.ejava.ca.rest;
+
+/**
+ *
+ * @author rzhao
+ */
+public class PeopleResource {
+    
+}
