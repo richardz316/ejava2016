e8af0c9a69f0e457c59dd59b8c482da3b5ade4a0
benjaminng <shen_soonng@hotmail.com>
2016-10-23 20:03:40 +0800

    resolve method not found

--- src/java/sg/edu/nus/iss/ejava/ca/rest/AppointmentResource.java
+++ src/java/sg/edu/nus/iss/ejava/ca/rest/AppointmentResource.java
@@ -5,19 +5,11 @@
  */
 package sg.edu.nus.iss.ejava.ca.rest;
 
-import java.util.List;
-import java.util.Optional;
 import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.context.RequestScoped;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,50 +17,26 @@ import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import sg.edu.nus.iss.ejava.ca.business.AppointmentBean;
-import sg.edu.nus.iss.ejava.ca.business.PeopleBean;
-import sg.edu.nus.iss.ejava.ca.model.Appointment;
-import sg.edu.nus.iss.ejava.ca.model.People;
 
 /**
  *
- * @author rzhao
+ * @author benjaminng
  */
 @RequestScoped
-@Path("/people")
-public class PeopleResource {
-    @EJB private PeopleBean peopleBean;
+@Path("/appointment")
+public class AppointmentResource {
     
     @EJB private AppointmentBean appointmentBean; 
-
     
     @Resource(mappedName = "concurrent/myThreadPool")
     private ManagedExecutorService executors;
     
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(MediaType.APPLICATION_JSON)
-    public void createPeople(MultivaluedMap<String, String> formData
-        , @Suspended AsyncResponse async) {
-        String name = formData.getFirst("name");
-	String email = formData.getFirst("email");
-        
-        CreatePeopleTask cpTask = new CreatePeopleTask();
-        cpTask.setPeopleData(name, email, peopleBean);
-        cpTask.setAsyncResponse(async);
-        
-        executors.execute(cpTask);
-        
-    }
-    
     @GET
+    @Path("{email}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void findAppointmentByEmail(@Context UriInfo info, @Suspended AsyncResponse async) {
-        
-        String email = info.getQueryParameters().getFirst("email");        
+    public void findAppointmentByEmail(@PathParam("email") String email, @Suspended AsyncResponse async) {      
         
         FetchAppointmentTask fptTask = new FetchAppointmentTask(); 
         fptTask.setPeopleData(email, appointmentBean);
@@ -93,7 +61,8 @@ public class PeopleResource {
 //                .entity("Email not found:" + email)
 //                .build());
     }
- 
+    
+    
     
     
 }
